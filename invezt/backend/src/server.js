520c58121b26593b25566240113e02c396d1bbf3
sanuli <sanuli.20231908@iit.ac.lk>
2026-02-22 22:04:20 +0530

    updated server.js

--- invezt/backend/src/server.js
+++ invezt/backend/src/server.js
@@ -1,33 +1,30 @@
-const dns = require('node:dns/promises');
-dns.setServers(['8.8.8.8', '1.1.1.1']); // Use Google and Cloudflare maps
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
 
-require('dotenv').config();
-const express = require('express');
-// ... the rest of your code
+import authenticationMiddleware from './middlewares/authenticationMiddleware.js';
+import authenticationRoutes from './routes/authenticationRoutes.js';
+import stockRoutes from './routes/stockRoutes.js';
 
-const mongoose = require('mongoose');
-const cors = require('cors');
-
+dotenv.config();
 const app = express();
 
-// 1. THE IMPORTS (Tell the computer where your files are)
-const authRoutes = require('./routes/authRoutes');
-const portfolioRoutes = require('./routes/portfolioRoutes');
-
-// Middleware - This allows the server to understand JSON sent from the frontend
+app.use(cors());
 app.use(express.json());
-app.use(cors()); // Allows your React frontend to talk to this backend
+app.use(express.urlencoded({extended: true}));
 
-// Connect to MongoDB
-mongoose.connect(process.env.MONGO_URI)
-    .then(() => console.log("✅ Connected to MongoDB"))
-    .catch(err => console.error("❌ Connection error:", err));
+app.use('/api/auth', authenticationMiddleware, authenticationRoutes);
+app.use('/api/stocks', authenticationMiddleware, stockRoutes);
 
-// Use Routes
-app.use('/api/auth', authRoutes);
-app.use('/api/portfolio', portfolioRoutes);
-app.use('/api/user', require('./routes/userRoutes'));
+app.get('/', (req, res) => {
+    res.status(200).json({
+        message: "invezt backend is running",
+        status: "ok",
+    });
+});
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+const PORT = process.env.PORT || 5001;
 
+app.listen(PORT, () => {
+    console.log(`server runnning on http://localhost:${PORT}`);
+});
