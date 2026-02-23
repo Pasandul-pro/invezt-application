a99076aa8259f1f297892f87818c6a746c2c6236
Sanuli Jayasinghe <sanuli.20231908@iit.ac.lk>
2026-02-23 16:37:34 +0530

    Update CORS configuration in server.js

--- invezt/backend/src/server.js
+++ invezt/backend/src/server.js
@@ -10,7 +10,15 @@ import stockRoutes from './routes/stockRoutes.js';
 dotenv.config();
 const app = express();
 
-app.use(cors());
+app.use(cors{
+    origin: process.env.FRONTEND_URL || "*",
+    credentials: false,
+    allowedHeaders: ["content-type", "authorzation"],
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+})
+);
+
+
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
