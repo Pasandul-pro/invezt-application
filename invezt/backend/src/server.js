7288b875a48c4be98cb5e217de9fa21deb646e4a
Sanuli Jayasinghe <sanuli.20231908@iit.ac.lk>
2026-02-23 16:12:34 +0530

    Add mongoose import to server.js

--- invezt/backend/src/server.js
+++ invezt/backend/src/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import mongoose from 'mongoose';
 
 import authenticationMiddleware from './middlewares/authenticationMiddleware.js';
 import authenticationRoutes from './routes/authenticationRoutes.js';
