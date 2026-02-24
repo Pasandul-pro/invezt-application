13ee8ba43e3b8e6cc56ebb6397800d5781737c9a
Sanuli Jayasinghe <sanuli.20231908@iit.ac.lk>
2026-02-24 16:19:37 +0530

    login route with email validation
    Refactor login route to use async/await and add email validation.

--- invezt/backend/src/routes/authenticationRoutes.js
+++ invezt/backend/src/routes/authenticationRoutes.js
@@ -1,7 +1,19 @@
 import express from 'express';
+import jwt from 'jsonwebtoken';
+
 const router = express.Router();
 
-router.post('/login', (req, res) => {
+router.post("/login", async(req, res) => {
+    try{
+        const {email} = req.body;
+
+        if(!email){
+            return res.status(400).json(
+                {
+                    status: "error",
+                    message: "enter your email",
+                });
+        }
     res.json({status:"success", message: "authentication route" });
 });
 
