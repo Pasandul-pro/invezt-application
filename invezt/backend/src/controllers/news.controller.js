a815f095b79358b4f4b78ff5a90cb96c74b2b22a
suv <suvindu.20240294@iit.ac.lk>
2026-03-10 17:10:11 +0530

    NO 0016 : backend news feature under news.controller.js inside base news cntroller class and the imports are created

---
+++ invezt/backend/src/controllers/news.controller.js
@@ -0,0 +1,9 @@
+import { summarizeNewsArticleFlow } from '../util/ai.flows.js';
+import { newsRepository } from '../util/news.repository.js';
+
+/**
+ * Controller for News-related routes.
+ */
+class NewsController {
+    
+}
