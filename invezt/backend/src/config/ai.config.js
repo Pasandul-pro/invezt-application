f3a7c3118a2876ab7a03631b3650a95261b5b160
suv <suvindu.20240294@iit.ac.lk>
2026-03-10 17:05:30 +0530

    NO 0015 : backend news feature under ai.config.js    file  has updated

---
+++ invezt/backend/src/config/ai.config.js
@@ -0,0 +1,10 @@
+import { googleAI } from '@genkit-ai/google-genai';
+import dotenv from 'dotenv';
+import { genkit } from 'genkit';
+
+dotenv.config();
+
+export const ai = genkit({
+    plugins: [googleAI()],
+    model: 'googleai/gemini-1.5-flash',
+});
