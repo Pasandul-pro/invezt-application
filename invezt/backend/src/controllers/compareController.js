762f03a59ca222b5c64c2743b99fd3dbeb46d94e
Sanuli Jayasinghe <sanuli.20231908@iit.ac.lk>
2026-03-11 13:21:59 +0530

    Add CSE trade summary URL constant

--- invezt/backend/src/controllers/compareController.js
+++ invezt/backend/src/controllers/compareController.js
@@ -2,3 +2,4 @@ import axios from "axios";
 import OpenAI from "openai";
 import Financials from "../models/financialDocumentModel.js";
 
+const cse_trade_summary_url = "https://www.cse.lk/api/tradeSummary";
