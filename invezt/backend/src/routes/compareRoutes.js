8f19f4bfd459278d8baf4b8895ff4e0d95fa7986
Pasandul Wickramaarachchi <thevinuomisha@gmail.com>
2026-03-12 05:14:54 +0530

    Remove compareRoutes.js
    Delete compareRoutes.js which defined an authenticated GET '/' route that called the compareCompanies controller. The route file has been removed from the codebase.

--- invezt/backend/src/routes/compareRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import authenticationMiddleware from "../middlewares/authenticationMiddleware.js";
-import { compareCompanies } from "../controllers/compareController.js";
-
-const router = express.Router();
-
-router.get("/", authenticationMiddleware, compareCompanies);
-export default router;
