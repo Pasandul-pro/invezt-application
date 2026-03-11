2eb8cab58939e2c866ba11714b7829dcb0b4e78e
Sanuli Jayasinghe <sanuli.20231908@iit.ac.lk>
2026-03-11 13:23:53 +0530

    Initialize OpenAI client with API key

--- invezt/backend/src/controllers/compareController.js
+++ invezt/backend/src/controllers/compareController.js
@@ -3,3 +3,4 @@ import OpenAI from "openai";
 import Financials from "../models/financialDocumentModel.js";
 
 const cse_trade_summary_url = "https://www.cse.lk/api/tradeSummary";
+const openai = new OpenAI ({ apiKey: process.env.openai_secret_key });
