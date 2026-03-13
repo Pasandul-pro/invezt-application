429281a7a45480b3ef0d4c7ca1f78ef8d53dc5cb
suv <suvindu.20240294@iit.ac.lk>
2026-03-14 00:50:30 +0530

    ews.service.test.js  test file has cbeen created firsthalf created
    ews.service.test.js  test file first half  add

---
+++ invezt/backend/tests/news.service.test.js
@@ -0,0 +1,5 @@
+import { newsService } from '../src/services/news.service.js';
+import { newsRepository } from '../src/repositories/news.repository.js';
+        expect(result.articles).toEqual([]);
+    });
+});
