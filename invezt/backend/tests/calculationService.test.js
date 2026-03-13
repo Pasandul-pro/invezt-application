7996becf36a26470296be1fca15611de631baa9b
suv <suvindu.20240294@iit.ac.lk>
2026-03-14 01:11:45 +0530

    calculationService.test.js  file created and add the test logic
    calculationService.test.js  file created and add the test logic

---
+++ invezt/backend/tests/calculationService.test.js
@@ -0,0 +1,9 @@
+import calculationService from '../src/services/calculationService.js';
+
+// Mock the models
+jest.mock('../src/models/financialDocumentModel.js');
+jest.mock('../src/models/calculatedRatioModel.js');
+jest.mock('../src/models/stockPriceModel.js');
+
+describe('CalculationService', () => {
+});
